52de760d88ce8b3a4d261a42098b86c5ddb36e5b
donaldamadii <donald@equivator.com>
2026-02-15 13:31:24 +0100

    Initial Commit

---
+++ GoPaddi/GoPaddiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoPaddiApp.swift
+//  GoPaddi
+//
+//  Created by Obinna Amadi on 15/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GoPaddiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoPaddiTests/GoPaddiTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoPaddiTests.swift
+//  GoPaddiTests
+//
+//  Created by Obinna Amadi on 15/02/2026.
+//
+
+import Testing
+@testable import GoPaddi
+
+struct GoPaddiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
